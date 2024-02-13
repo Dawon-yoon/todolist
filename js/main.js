2262b4e38164381ef03fcc4c3a3408549d421de5
wonyhayashi <154343298+Dawon-yoon@users.noreply.github.com>
2024-02-13 20:23:07 +0900

    #2

--- js/main.js
+++ js/main.js
@@ -5,8 +5,12 @@ let taskBoard = document.getElementById("task-board");
 let todoList = [];
 
 function addTodoList() {
-  let taskConten = userInput.value;
-  todoList.push(taskConten);
+  let task = {
+    id: randomId(),
+    taskContent: userInput.value,
+    isComplete: false,
+  };
+  todoList.push(task);
   console.log(todoList);
   render();
 }
@@ -14,15 +18,51 @@ function addTodoList() {
 function render() {
   let resultHTML = "";
   for (let i = 0; i < todoList.length; i++) {
-    resultHTML += ` <div class="task">
-        <div>${todoList[i]} </div>
+    if (todoList[i].isComplete === true) {
+      resultHTML += `<div class="task task-grey">
+      <div class="task-done">${todoList[i].taskContent} </div>
+      <div>
+        <button onclick="toggleComplete('${todoList[i].id}')"><i class="fa-solid fa-rotate-left"></i></button>
+        <button onclick="toggleDelete('${todoList[i].id}')"><i class="fa-solid fa-trash"></i></button>
+      </div>
+    </div>`;
+    } else {
+      resultHTML += ` <div class="task">
+        <div>${todoList[i].taskContent} </div>
         <div>
-          <button><i class="fa-solid fa-check"></i></button>
-          <button><i class="fa-solid fa-trash"></i></button>
+          <button onclick="toggleComplete('${todoList[i].id}')"><i class="fa-solid fa-check"></i></button>
+          <button onclick="toggleDelete('${todoList[i].id}')"><i class="fa-solid fa-trash"></i></button>
         </div>
       </div>`;
+    }
   }
   taskBoard.innerHTML = resultHTML;
+}
+
+function toggleComplete(id) {
+  console.log("id:", id);
+  for (let i = 0; i < todoList.length; i++) {
+    if (todoList[i].id === id) {
+      todoList[i].isComplete = !todoList[i].isComplete;
+      break;
+    }
+  }
+  console.log(todoList);
+  render();
+}
+
+function toggleDelete(id) {
+  for (let i = 0; i < todoList.length; i++) {
+    if (todoList[i].id === id) {
+      todoList.splice(i, 1);
+      break;
+    }
+  }
+  render();
+}
+
+function randomId() {
+  return "_" + Math.random().toString(36).substring(2, 9);
 }
 
 addButton.addEventListener("click", addTodoList);
